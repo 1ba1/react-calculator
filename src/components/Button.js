fd4dd6e4c17eebe6dbb9263a6394a3da424f0b99
1ba1 <vvvalenti01@yahoo.it>
2019-07-24 14:34:17 +0200

    second milestone

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Button = (props) => {
+  return <button>{props.name}</button>;
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+}
---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Display = (props) => {
+  return <div>{props.result}</div>;
+}
+
+Display.propTypes = {
+  result: PropTypes.string,
+}
+
+Display.defaultProps = {
+  result: '0',
+}
