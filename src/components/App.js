5eeb673872d46f5de4c261f0cc32a2f97540d125
1ba1 <vvvalenti01@yahoo.it>
2019-07-25 12:07:00 +0200

    refactor App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,11 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state.total = null;
-  }
+const App = () => (
+  <div id="App">
+    <Display />
+    <ButtonPanel />
+  </div>
+);
 
-  render() {
-    return (
-      <div id="App">
-        <Display />
-        <ButtonPanel />
-      </div>
-    );
-  }
-}
+export default App;
