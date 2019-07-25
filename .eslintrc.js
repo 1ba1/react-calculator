613c09ad5962b837db49f7efd52577abfdb4a5fb
1ba1 <vvvalenti01@yahoo.it>
2019-07-25 11:47:44 +0200

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-    'import/no-unresolved': off,
+    'import/no-unresolved': 'off',
   },
 };
